ff0f51ad4111f2c90027ff4bf24ff04a27b469d3
Bogdan Ratiu <bratiu@lenovo.com>
2020-01-23 18:56:01 +0200

    Changed the command result from dictionary to list in order to preserve the order.

--- arista_cnos_jsonrpc_comparation.py
+++ arista_cnos_jsonrpc_comparation.py
@@ -4,31 +4,46 @@ import pandas
 from devices import *
 from jsonrpclib import Server
 
-def add_header(f):
-    header = open("header.txt", "r")
-    for line in header:
-        f.write(line)
+def beautify (output, f):
 
+    def add_header(f):
+        header = open("header.txt", "r")
+        for line in header:
+            f.write(line)
 
-def beautify (elem, indent, f):
-    f.write(json.dumps(elem, indent=4, sort_keys=True).replace('\n', '<br>').replace(' ', "&nbsp"))
+    add_header(f)
+    f.write("<tbody>\n")
+    for i in range(len(output["arista"])):
+        command = list(output["arista"][i].keys())[0]
+        f.write("<tr style=\"text-align: left;\">\n")
+        f.write("<th>")
+        f.write(command)
+        f.write("</th>\n")
+        f.write("<td>")
+        f.write(json.dumps(output['arista'][i][command], indent=4, sort_keys=True).replace('\n', '<br>').replace(' ', "&nbsp"))
+        f.write("</td>\n")
+        f.write("<td>")
+        f.write(json.dumps(output['cnos'][i][command], indent=4, sort_keys=True).replace('\n', '<br>').replace(' ', "&nbsp"))
+        f.write("</td>\n")
+        f.write("</tr>\n")
+    f.write("</tbody>\n")
+    f.write("</table>\n")
+
 
 def cmd_run(cnos, arista, output, cmd):
     print "Running \"%s\" command on cnos." % cmd
     cnos_output = cnos.runCmds(cmd)
     print "Running \"%s\" command on arista." % cmd
     arista_output = arista.runCmds(1, cmd)
-    # output[cmd]={}
-    # output[cmd]['cnos']=cnos_output
-    # output[cmd]['arista']=arista_output
-    output['arista'][str(cmd)] = arista_output
-    output['cnos'][str(cmd)] = cnos_output
+    output['arista'].append({str(cmd) : arista_output})
+    output['cnos'].append({str(cmd) : cnos_output})
 
 
 cnos = Server(cnos_url)
 arista = Server(arista_url)
 
-output = {"arista": {}, "cnos": {}}
+#changed to list so that the order of command lines stays the same when printing
+output = {"arista": [], "cnos": []}
 
 with open('command_list.txt', 'r') as cmd_file:
     for line in cmd_file:
@@ -53,18 +68,5 @@ df.to_excel(writer, sheet_name='Sheet1')
 writer.save()
 
 f = open("info.html", "w")
-add_header(f)
-print("Calling beautify!")
-for command in output["arista"].keys():
-    f.write("<tr style=\"text-align: left;\">\n")
-    f.write("<th>")
-    f.write(command)
-    f.write("</th>\n")
-    f.write("<th>")
-    beautify(output['arista'][command], indent=0, f=f)
-    f.write("</th>\n")
-    f.write("<th>")
-    beautify(output['cnos'][command], indent=0, f=f)
-    f.write("</th>\n")
-    f.write("</tr>\n")
+beautify(output, f)
 f.close()
--- devices.py
+++ devices.py
@@ -1,2 +1,2 @@
-cnos_url = 'http://10.241.8.40:8080'
+cnos_url = 'http://10.241.7.145:8080'
 arista_url = 'http://admin:admin@192.168.10.120/command-api'
